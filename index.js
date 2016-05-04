7aa3b49280ab75dc07ab77a146a2a21285a2c12b
Don Bencilao <Don Bencilao@egg.ph>
2016-05-04 14:34:03 +0800

    eee

--- index.js
+++ index.js
@@ -1,20 +1,60 @@
 var express = require('express'),
 	app = express(),
 	server = require('http').createServer(app),
-	io = require('socket.io').listen(server);
+	io = require('socket.io')(server),
+	port = process.env.PORT || 3000;
 
 	var nicknames = {};
 	nicknames.name = [];
-	server.listen(3000);
+
+	server.listen(port,function(){
+		console.log('server listening at port',port);
+	});
 
 	app.get('/',function(req,res){
 		res.sendFile(__dirname + '/index.html');
 	});
 
+	// var nsp = io.of('/my-namespace');
+	// nsp.on('connection', function(socket){
+
+	//   socket.on('set nickname',function(data,callback){
+			
+	// 		if(nicknames.name.indexOf(data) != -1){
+	// 			callback(false);
+	// 		}else{
+	// 			callback(true);
+	// 			socket.nickname = data;
+	// 			nicknames.name.push(data);
+	// 			updateUserList();
+	// 		}
+	// 	});
+
+	// 	function updateUserList(){
+	// 		nsp.emit('usernames',nicknames.name);
+	// 	}
+
+	// 	socket.on('send message', function(data){
+
+	// 		nsp.emit('new message',{'msg':data,'name':socket.nickname});
+	// 	});
+
+	// 	socket.on('disconnect',function(){
+	// 		if(!socket.nickname) return;
+	// 		nicknames.name.splice(nicknames.name.indexOf(socket.nickname),1);
+	// 		updateUserList();
+	// 	});
+	// });
+
+
 	io.sockets.on('connection',function(socket){
 		
-		socket.on('set nickname',function(data,callback){
-			
+		
+		io.to('some room').emit('some event');
+
+		socket.on('set nickname',function(data,room,callback){
+			console.log(room);
+			socket.join(room);
 			if(nicknames.name.indexOf(data) != -1){
 				callback(false);
 			}else{
@@ -24,6 +64,8 @@ var express = require('express'),
 				updateUserList();
 			}
 		});
+
+
 
 		function updateUserList(){
 			io.sockets.emit('usernames',nicknames.name);
