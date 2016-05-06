10d776d1e30fa7b267f31d13622b4fc4c595d9d9
Don Bencilao <Don Bencilao@egg.ph>
2016-05-06 11:06:20 +0800

    join room

--- index.js
+++ index.js
@@ -16,49 +16,23 @@ var express = require('express'),
 	});
 
 
-	io.sockets.on('connection',function(socket){
-		connection.push(socket);
-		console.log('connections: %s', connection.length);
+	io.on('connection', function(socket){
 
-		//Globals
-	  	var defaultRoom = 'general';
-	  	var rooms = ["General", "angular", "socket.io", "express", "node", "mongo", "PHP", "laravel"];
+	  socket.on('switch room',function(room){
+	  	socket.leave(room);
+	  	socket.join(room);
+	  	console.log(room);
+	  	io.to(room).emit('some event',`hey! room ${room}` );
+	  });
+	  
+	  socket.on('send message',function(data){
+	  	socket.emit('new message',data);
+	  });
 
-		socket.on('set nickname',function(data,room,callback){
-			if(data in users){
-				callback(false);
-			}else{
-				callback(true);
-			    //New user joins the default room
-			    socket.join(defaultRoom);
-			    //console.log(data.room);
-				socket.nickname = data;
-				users[socket.nickname] = socket;
-				updateUserList();
-			}
-		});
-
-		function updateUserList(){
-			io.sockets.emit('usernames',Object.keys(users));
-		}
-
-		socket.on('send message', function(data){
-			io.sockets.emit('new message',{'msg':data,'name':socket.nickname});
-		});
-
-		socket.on('switch room',function(data){
-			socket.leave(defaultRoom);
-			socket.join(a);
-		});
-
-		// Disconnect
-		socket.on('disconnect',function(){
-			console.log('connections: %s', connection.length);
-			if(!socket.nickname) return;
-			delete users[socket.nickname];
-			updateUserList();
-		});
 	});
+
+
+
 
 
 
