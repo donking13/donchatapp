804fd3cf15ec17b0afc1de7b9c5452763ea50a4c
Don Bencilao <Don Bencilao@egg.ph>
2016-05-06 09:19:12 +0800

    stable

--- index.js
+++ index.js
@@ -4,81 +4,61 @@ var express = require('express'),
 	io = require('socket.io')(server),
 	port = process.env.PORT || 3000;
 
-	var nicknames = {};
-	nicknames.name = [];
+	var users = {},
+		connection = [];
 
 	server.listen(port,function(){
-		console.log('server listening at port',port);
+		console.log('server listening at port %s',port);
 	});
 
 	app.get('/',function(req,res){
 		res.sendFile(__dirname + '/index.html');
 	});
 
-	// var nsp = io.of('/my-namespace');
-	// nsp.on('connection', function(socket){
-
-	//   socket.on('set nickname',function(data,callback){
-			
-	// 		if(nicknames.name.indexOf(data) != -1){
-	// 			callback(false);
-	// 		}else{
-	// 			callback(true);
-	// 			socket.nickname = data;
-	// 			nicknames.name.push(data);
-	// 			updateUserList();
-	// 		}
-	// 	});
-
-	// 	function updateUserList(){
-	// 		nsp.emit('usernames',nicknames.name);
-	// 	}
-
-	// 	socket.on('send message', function(data){
-
-	// 		nsp.emit('new message',{'msg':data,'name':socket.nickname});
-	// 	});
-
-	// 	socket.on('disconnect',function(){
-	// 		if(!socket.nickname) return;
-	// 		nicknames.name.splice(nicknames.name.indexOf(socket.nickname),1);
-	// 		updateUserList();
-	// 	});
-	// });
-
 
 	io.sockets.on('connection',function(socket){
-		
-		
-		io.to('some room').emit('some event');
+		connection.push(socket);
+		console.log('connections: %s', connection.length);
+
+		//Globals
+	  	var defaultRoom = 'general';
+	  	var rooms = ["General", "angular", "socket.io", "express", "node", "mongo", "PHP", "laravel"];
 
 		socket.on('set nickname',function(data,room,callback){
-			console.log(room);
-			socket.join(room);
-			if(nicknames.name.indexOf(data) != -1){
+			if(data in users){
 				callback(false);
 			}else{
 				callback(true);
+			    //New user joins the default room
+			    socket.join(defaultRoom);
+			    //console.log(data.room);
 				socket.nickname = data;
-				nicknames.name.push(data);
+				users[socket.nickname] = socket;
 				updateUserList();
 			}
 		});
 
-
-
 		function updateUserList(){
-			io.sockets.emit('usernames',nicknames.name);
+			io.sockets.emit('usernames',Object.keys(users));
 		}
 
 		socket.on('send message', function(data){
-
 			io.sockets.emit('new message',{'msg':data,'name':socket.nickname});
 		});
 
+		socket.on('switch room',function(data){
+			socket.leave(defaultRoom);
+			socket.join(a);
+		});
+
+		// Disconnect
 		socket.on('disconnect',function(){
+			console.log('connections: %s', connection.length);
 			if(!socket.nickname) return;
-			nicknames.name.splice(nicknames.name.indexOf(socket.nickname),1);
+			delete users[socket.nickname];
 			updateUserList();
 		});
 	});
+
+
+
